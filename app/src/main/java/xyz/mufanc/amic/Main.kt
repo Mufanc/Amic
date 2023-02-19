3e17b36bae195d30191255d409ba8f386a4ec74f
Mufanc <mufanc.xyz@gmail.com>
2023-02-19 16:31:09 +0800

    feat: delete apk when fallback to extract mode

--- app/src/main/java/xyz/mufanc/amic/Main.kt
+++ app/src/main/java/xyz/mufanc/amic/Main.kt
@@ -1,8 +1,44 @@
 package xyz.mufanc.amic
 
+import android.annotation.SuppressLint
+import dalvik.system.BaseDexClassLoader
+import java.io.File
+
+fun catch(block: () -> Unit) {
+    try {
+        block()
+    } catch (err: Throwable) {
+        error(err)
+    }
+}
+
+@SuppressLint("DiscouragedPrivateApi")
 object Main {
+    init {
+        BaseDexClassLoader::class.java.getDeclaredField("pathList")
+            .apply { isAccessible = true }
+            .get(javaClass.classLoader)
+            .let { pathList ->
+                pathList.javaClass.getDeclaredField("dexElements")
+                    .apply { isAccessible = true }
+                    .get(pathList)
+                    .let { it as Array<*> }
+                    .first()!!
+                    .let { element ->
+                        element.javaClass.getDeclaredField("path")
+                            .apply { isAccessible = true }
+                            .get(element)
+                    }
+            }
+            .let { it as File }
+            .takeIf { it.path == "/data/local/tmp/.amic.apk" }
+            ?.delete()
+    }
+
     @JvmStatic
     fun main(vararg args: String) {
-        println("Hello: ${args.contentToString()}")
+        catch {
+            println("Hello: ${args.contentToString()}")
+        }
     }
 }
